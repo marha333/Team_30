bd833b93781afd48c48614b98afa5f3c950ed132
Johannes Erlacher <johannes.erlacher@protonmail.com>
2021-04-09 14:41:35 +0200

    SR-014 Sample MainActivity and setup dependency injection [EJ, LB]

---
+++ app/src/main/java/at/team30/setroute/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package at.team30.setroute.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import at.team30.setroute.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
