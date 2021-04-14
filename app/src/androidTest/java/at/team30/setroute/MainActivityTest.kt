4622c8c6e00a2255af8b4e1ae670746d46c93d33
Johannes Erlacher <johannes.erlacher@protonmail.com>
2021-04-14 10:11:07 +0200

    SR-014 Add Espresso frameword, implement example [EJ, LB]

---
+++ app/src/androidTest/java/at/team30/setroute/MainActivityTest.kt
@@ -0,0 +1,25 @@
+package at.team30.setroute
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import at.team30.setroute.ui.MainActivity
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class MainActivityTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun helloWorldIsDisplayed() {
+        onView(withText("Hello World")).check(matches(isDisplayed()))
+    }
+}
