bf32a20dfa0ad8eeefed1f73e1a9961af1bdeaf7
harald.martini@student.tugraz.at <harald.martini@student.tugraz.at>
2021-04-14 17:55:06 +0200

    SR-022
    Added Maps Fragment and Google Maps API Key.
    
    It is assumed that the permission is granted (See SR007) for debug purposes add -g install flag to automatically grant all requested permissions.
    
    Location currently not working in emulator.
    
    add MAPS_API_KEY=<---MAPS API KEY----> in local.properties
    
    [BL,HM,OP]

---
+++ app/src/main/java/at/team30/setroute/ui/MapsFragment.kt
@@ -0,0 +1,59 @@
+package at.team30.setroute.ui
+
+import android.annotation.SuppressLint
+import androidx.fragment.app.Fragment
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import at.team30.setroute.R
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapsFragment : Fragment() {
+
+    private lateinit var mMap: GoogleMap
+    @SuppressLint("MissingPermission")
+    private val callback = OnMapReadyCallback { googleMap ->
+        /**
+         * Manipulates the map once available.
+         * This callback is triggered when the map is ready to be used.
+         * This is where we can add markers or lines, add listeners or move the camera.
+         * In this case, we just add a marker near Sydney, Australia.
+         * If Google Play services is not installed on the device, the user will be prompted to
+         * install it inside the SupportMapFragment. This method will only be triggered once the
+         * user has installed Google Play services and returned to the app.
+         */
+
+
+
+
+
+        mMap = googleMap
+        mMap.isMyLocationEnabled=true;
+    }
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_maps, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
