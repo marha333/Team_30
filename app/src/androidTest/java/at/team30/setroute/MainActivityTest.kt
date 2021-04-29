76fb32da66f9e83b2266c4b46a1b8d07b9e6db55
Johannes Erlacher <johannes.erlacher@protonmail.com>
2021-04-29 12:26:16 +0200

    SR-027 Improve testing framework, introduce mocking [PS, PO, EJ]

--- app/src/androidTest/java/at/team30/setroute/MainActivityTest.kt
+++ app/src/androidTest/java/at/team30/setroute/MainActivityTest.kt
@@ -1,31 +1,132 @@
 package at.team30.setroute
 
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.pressBack
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.ext.junit.rules.ActivityScenarioRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.filters.LargeTest
+import androidx.test.espresso.matcher.ViewMatchers.*
+import at.team30.setroute.infrastructure.DependencyInjection
+import at.team30.setroute.infrastructure.IRoutesRepository
+import at.team30.setroute.models.Route
 import at.team30.setroute.ui.MainActivity
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import io.mockk.every
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+import javax.inject.Inject
 
-@RunWith(AndroidJUnit4::class)
-@LargeTest
+@UninstallModules(DependencyInjection::class)
+@HiltAndroidTest
 class MainActivityTest {
 
-    @get:Rule
-    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+    @get:Rule(order = 0)
+    var hiltRule = HiltAndroidRule(this)
+
+    @Inject
+    lateinit var mockRepository: IRoutesRepository
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
 
     @Test
-    fun routes_displayed() {
-        onView(withText("Coffee")).check(matches(isDisplayed()))
-        onView(withText("Coffee2")).check(matches(isDisplayed()))
-        onView(withText("Parks")).check(matches(isDisplayed()))
-        onView(withText("Parks2")).check(matches(isDisplayed()))
+    fun given_one_route_in_repository_expect_one_route_displayed_in_list() {
+        // Arrange
+        given_routes_in_repository(FixturesAndroid.routes_one())
 
-        //Other routes not displayed on the emulator display, you need to scroll to reach it
+        // Act
+        ActivityScenario.launch(MainActivity::class.java)
+
+        // Assert
+        for(route in FixturesAndroid.routes_one()) {
+            onView(withText(route.name)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun given_many_routes_in_repository_expect_all_routes_displayed_in_list() {
+        // Arrange
+        given_routes_in_repository(FixturesAndroid.routes_many())
+
+        // Act
+        ActivityScenario.launch(MainActivity::class.java)
+
+        // Assert
+        for(route in FixturesAndroid.routes_many()) {
+            onView(withText(route.name)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun given_many_routes_enter_detail_of_first_route() {
+        // Arrange
+        given_routes_in_repository(FixturesAndroid.routes_many())
+        val selectedRoute = FixturesAndroid.routes_many()[0]
+
+        // Act
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withText(selectedRoute.name)).perform(click())
+
+        // Assert
+        onView(withText(selectedRoute.name)).check(matches(isDisplayed()))
+        onView(withText(selectedRoute.description)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun given_many_routes_enter_detail_press_back_displays_list_view() {
+        // Arrange
+        given_routes_in_repository(FixturesAndroid.routes_many())
+        val selectedRoute = FixturesAndroid.routes_many()[0]
+
+        // Act
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withText(selectedRoute.name)).perform(click())
+
+        // Assert
+        onView(withText(selectedRoute.name)).check(matches(isDisplayed()))
+        onView(withText(selectedRoute.description)).check(matches(isDisplayed()))
+
+        // Act
+        onView(isRoot()).perform(pressBack())
+
+        // Assert
+        for(route in FixturesAndroid.routes_many()) {
+            onView(withText(route.name)).check(matches(isDisplayed()))
+        }
+    }
+
+    @Test
+    fun given_many_routes_enter_detail_press_routes_button_displays_list_view() {
+        // Arrange
+        given_routes_in_repository(FixturesAndroid.routes_many())
+        val selectedRoute = FixturesAndroid.routes_many()[0]
+
+        // Act
+        ActivityScenario.launch(MainActivity::class.java)
+        onView(withText(selectedRoute.name)).perform(click())
+
+        // Assert
+        onView(withText(selectedRoute.name)).check(matches(isDisplayed()))
+        onView(withText(selectedRoute.description)).check(matches(isDisplayed()))
+
+        // Act
+        onView(withId(R.id.routesFragment)).perform(click())
+
+        // Assert
+        for(route in FixturesAndroid.routes_many()) {
+            onView(withText(route.name)).check(matches(isDisplayed()))
+        }
+    }
+
+    private fun given_routes_in_repository(routes: List<Route>) {
+        every { mockRepository.getRoutes() } returns routes
+        for (route in routes) {
+            every { mockRepository.getRoutesById(route.id) } returns route
+        }
     }
 }
